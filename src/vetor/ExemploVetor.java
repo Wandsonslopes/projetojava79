0c78ffed364cd3ef5b72cf38847071e2612c4d5f
Wandsonslopes <wandson_lopes@hotmail.com>
2024-12-10 08:28:45 -0300

    Exemplos de Collections

---
+++ src/vetor/ExemploVetor.java
@@ -0,0 +1,16 @@
+package vetor;
+
+public class ExemploVetor {
+
+	public static void main(String[] args) {
+
+		String cachorros[] = { "Boxer", "Pastor Alemão", "Pinscher", "Husky Siberiano", "Corgi" };
+		// Tipo nome_vetor[] = { }
+
+		for (int indice = 0; indice < 5; indice++) {
+			System.out.println((indice + 1) + "º elemento: " + cachorros[indice]);
+		}
+
+	}
+
+}
